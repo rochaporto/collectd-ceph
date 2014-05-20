5b8c9eb18c430928a2f7aef25811ce4d0dc25f45
Ricardo Rocha <rocha.porto@gmail.com>
2014-05-20 17:04:39 +1200

    added placement group dump (pg stats) #3.
    adds a new plugin pushing data from ceph pg dump, mostly gathering stats
    on the states of the multiple placement groups.

---
+++ plugins/ceph_pg_plugin.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python
+#
+# vim: tabstop=4 shiftwidth=4
+
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the
+# Free Software Foundation; only version 2 of the License is applicable.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along
+# with this program; if not, write to the Free Software Foundation, Inc.,
+# 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+#
+# Authors:
+#   Ricardo Rocha <ricardo@catalyst.net.nz>
+#
+# About this plugin:
+#   This plugin collects information regarding Ceph Placement Groups (PGs).
+#
+# collectd:
+#   http://collectd.org
+# collectd-python:
+#   http://collectd.org/documentation/manpages/collectd-python.5.shtml
+#
+
+import collectd
+import json
+import traceback
+import subprocess
+
+import base
+
+class CephPGPlugin(base.Base):
+
+    def __init__(self):
+        base.Base.__init__(self)
+        self.prefix = 'ceph'
+
+    def get_stats(self):
+        """Retrieves stats from ceph pgs"""
+
+        data = { self.prefix: { self.cluster: { 'pg': { } } } }
+        output = None
+        try:
+            output = subprocess.check_output(['ceph', 'pg', 'dump', '--format', 'json'])
+        except Exception as exc:
+            collectd.error("ceph-pg: failed to ceph pg dump :: %s :: %s"
+                    % (exc, traceback.format_exc()))
+            return
+
+        if output is None:
+            collectd.error('ceph-pg: failed to ceph osd dump :: output was None')
+
+        json_data = json.loads(output)
+
+        pg_data = data[self.prefix][self.cluster]['pg']
+        # number of pgs in each possible state
+        for pg in json_data['pg_stats']:
+            for state in pg['state'].split('+'):
+                if not pg_data.has_key(state):
+                    pg_data[state] = 0
+                pg_data[state] += 1
+    
+        return data
+
+try:
+    plugin = CephPGPlugin()
+except Exception as exc:
+    collectd.error("ceph-pg: failed to initialize ceph pg plugin :: %s :: %s"
+            % (exc, traceback.format_exc()))
+
+def configure_callback(conf):
+    """Received configuration information"""
+    plugin.config_callback(conf)
+
+def read_callback():
+    """Callback triggerred by collectd on read"""
+    plugin.read_callback()
+
+collectd.register_config(configure_callback)
+collectd.register_read(read_callback)
+
